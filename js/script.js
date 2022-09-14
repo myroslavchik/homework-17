35d2d4416f88017c25995b2a79706666c6a7c987
Myroslav <myroslavtp@gmail.com>
2022-09-14 18:08:01 +0300

    changes

--- js/script.js
+++ js/script.js
@@ -13,71 +13,121 @@
 // необхідно робити перерву на 1 годину. 
 
 
-let car = {
-    producer: 'Volkswagen',
-    model: 'Golf VII',
-    year: 2019,
-    speed: 100,
-    fuelTank: 55,
-    fuelConsumsion: '6 liters',
-    driverOne: 'Anna',
-    driverTwo: 'Oleg',
-    newDriver: function (driver) {
-        this.newDriver = driver;
-    },
+// let car = {
+//     producer: 'Volkswagen',
+//     model: 'Golf VII',
+//     year: 2019,
+//     speed: 100,
+//     fuelTank: 55,
+//     fuelConsumsion: '6 liters',
+//     driverOne: 'Anna',
+//     driverTwo: 'Oleg',
+//     newDriver: function (driver) {
+//         this.newDriver = driver;
+//     },
 
-    checkDriver: function (name) {
-        if (name === this.driverOne || name === this.driverTwo || name ===  this.newDriver) {
-            return 'Водій існує!';
-        } else return 'Немає в базі такого водія!';
-    },
+//     checkDriver: function (name) {
+//         if (name === this.driverOne || name === this.driverTwo || name ===  this.newDriver) {
+//             return 'Водій існує!';
+//         } else return 'Немає в базі такого водія!';
+//     },
 
-    checkTrip: function (distance) {
-        let time;
-        let fuel;
-       if(distance) {
-            time = distance / this.speed;
-            fuel = (distance / 100) * 6;
-            if(time % 4 === 0) {
-                time = time + 1;
-            }
-       } else return 0;
+//     checkTrip: function (distance) {
+//         let time;
+//         let fuel;
+//        if(distance) {
+//             time = distance / this.speed;
+//             fuel = (distance / 100) * 6;
+//             if(time % 4 === 0) {
+//                 time = time + 1;
+//             }
+//        } else return 0;
 
-       return `Необхідний час: ${time} год. і палива: ${fuel} літрів.`;
-    }
-};
-////
-car.carInform = function() {
-    return `${car.producer} ${car.model},
-    ${car.year}, avarage speed: ${car.speed},
-    fuel tank volume: ${car.fuelTank},
-    consumption per 100 kilometers: ${car.fuelConsumsion}`
-};
+//        return `Необхідний час: ${time} год. і палива: ${fuel} літрів.`;
+//     }
+// };
+// ////
+// car.carInform = function() {
+//     return `${car.producer} ${car.model},
+//     ${car.year}, avarage speed: ${car.speed},
+//     fuel tank volume: ${car.fuelTank},
+//     consumption per 100 kilometers: ${car.fuelConsumsion}`
+// };
   
-console.log(car.carInform());
+// console.log(car.carInform());
 
-/////
+// /////
 
-car.newDriver('Vova');
-console.log(car.newDriver);
+// car.newDriver('Vova');
+// console.log(car.newDriver);
 
-////
+// ////
 
-console.log(car.checkDriver('Anna'));
-console.log(car.checkDriver('Piter'));
-console.log(car.checkDriver('Potter'));
+// console.log(car.checkDriver('Anna'));
+// console.log(car.checkDriver('Piter'));
+// console.log(car.checkDriver('Potter'));
 
-///
+// ///
 
-console.log(car.checkTrip(400));
-console.log(car.checkTrip(500));
-console.log(car.checkTrip(600));
-console.log(car.checkTrip(800));
-console.log(car.checkTrip(1000));
-console.log(car.checkTrip(1200));
-
-
+// console.log(car.checkTrip(400));
+// console.log(car.checkTrip(500));
+// console.log(car.checkTrip(600));
+// console.log(car.checkTrip(800));
+// console.log(car.checkTrip(1000));
+// console.log(car.checkTrip(1200));
 
 
 
+// Норма
 
+// Створити об'єкт, що описує час (години, хвилини, секунди),
+// і такі функції для роботи з цим об'єктом:
+// Для виведення часу на екран.
+// Зміни часу на передану кількість секунд.
+// Зміни часу на передану кількість хвилин.
+// Зміни часу на передану кількість годин.
+// Враховуйте, що в останніх 3-х функціях,
+// при зміні однієї частини часу, може змінитися і інша.
+// Наприклад: якщо до часу «20:59:45» додати 30 секунд,
+// то повинно вийти «21:00:15», а не «20:59:75».
+// Також потрібно передбачити можливість того що
+// користувач може передати 150 секунд, або 75 хвилин.
+
+// let time = {
+//     hours: 23,
+//     minutes: 59,
+//     seconds: 59,
+
+//     timeIs: function () {
+//         return `Time: ${this.hours}:${this.minutes}:${this.seconds}`
+//     },
+
+//     changeSeconds: function (plusSeconds) {
+//         if (plusSeconds) {
+//                 this.seconds = plusSeconds;
+//             }
+//         return time.timeIs()
+//     },
+
+//     changeMinutes: function (plusMinutes) {
+//         if (plusMinutes) {
+//             this.minutes = plusMinutes;
+//         }
+//         return time.timeIs()
+//     },
+
+//     changeHours: function (plusHours) {
+//         if (plusHours > 23) {
+//             return `Невірні дані`
+//         }
+//         this.hours = plusHours;
+//         return time.timeIs()
+//     }
+
+// }
+
+
+// console.log(time.timeIs());
+// console.log(time.changeSeconds(30));
+// console.log(time.changeMinutes(30));
+// console.log(time.changeHours(22));
